38c754e9b5e2148af82376b4c57f4c620cee39eb
Thijs Moens <post@thijs.email>
2022-07-09 14:27:09 +0200

    👌 IMPROVE: Add check for input

--- src/camp/thijs/mortgageCalculator.java
+++ src/camp/thijs/mortgageCalculator.java
@@ -11,6 +11,10 @@ public class mortgageCalculator {
         // Create constants
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+        
+        int principal = 0;
+        float annualInterestRate = 0;
+        byte period = 0;
 
         // create Scanner object for Input
         Scanner inputUser = new Scanner(System.in); 
@@ -18,21 +22,47 @@ public class mortgageCalculator {
         /*
          * Step 1: Ask for principal form user
          */
-        System.out.print("Principal: "); // prompt the user for an input
-        int principal = inputUser.nextInt(); // Input field for principal
-    
+         while(true) {
+            System.out.print("Principal (1K - 1M): "); 
+            principal = inputUser.nextInt();
+
+            // Check for valid input
+            if(principal >= 1000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
+
         /*
          * Step 2: Ask for Annual Interest Rate
          */
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = inputUser.nextFloat(); 
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = inputUser.nextFloat(); 
+
+            // Check for valid input
+            if(annualInterestRate < 30) {
+                break;              
+            } 
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+
         float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR; // Calculate monthly Interest Rate
+
 
         /*
          * Step 3: Ask for the period in Years
          */
-        System.out.print("Period (Years): ");
-        byte period = inputUser.nextByte(); 
+        while(true) {
+            System.out.print("Period (Years): ");
+            period = inputUser.nextByte(); 
+
+            // Check for valid input
+            if(period < 30) {
+                break;         
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
         int numberOfPayments = period * MONTHS_IN_YEAR; // Calculate monthly payments
 
         /*
