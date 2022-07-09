7aeedee948b2433344edb8f161f4e7009bcc432d
Thijs Moens <post@thijs.email>
2022-07-09 17:51:49 +0200

    👌 IMPROVE: Refactor of the code

--- src/camp/thijs/mortgageCalculator.java
+++ src/camp/thijs/mortgageCalculator.java
@@ -7,14 +7,10 @@ import java.util.Currency;
 public class mortgageCalculator {
 
     public static void main(String[] args) {
-
-        // Create constants
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
         
         int principal = 0;
-        float annualInterestRate = 0;
         byte period = 0;
+        float annualInterestRate = 0;
 
         // create Scanner object for Input
         Scanner inputUser = new Scanner(System.in); 
@@ -27,9 +23,8 @@ public class mortgageCalculator {
             principal = inputUser.nextInt();
 
             // Check for valid input
-            if(principal >= 1000 && principal <= 1_000_000) {
+            if(principal >= 1000 && principal <= 1_000_000) 
                 break;
-            }
             System.out.println("Enter a number between 1,000 and 1,000,000");
         }
 
@@ -41,13 +36,10 @@ public class mortgageCalculator {
             annualInterestRate = inputUser.nextFloat(); 
 
             // Check for valid input
-            if(annualInterestRate < 30) {
+            if(annualInterestRate < 30) 
                 break;              
-            } 
             System.out.println("Enter a value greater than 0 and less than or equal to 30");
         }
-
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR; // Calculate monthly Interest Rate
 
 
         /*
@@ -58,24 +50,16 @@ public class mortgageCalculator {
             period = inputUser.nextByte(); 
 
             // Check for valid input
-            if(period < 30) {
+            if(period <= 30) 
                 break;         
-            }
+            
             System.out.println("Enter a value between 1 and 30");
         }
-        int numberOfPayments = period * MONTHS_IN_YEAR; // Calculate monthly payments
 
         /*
          * Step 4: Calculate Mortgage
          */
-        float monthlyInterestRatePlusOne = 1 + monthlyInterestRate;
-        double upperSum = Math.pow(monthlyInterestRatePlusOne, numberOfPayments); 
-        double upperSumFinal = monthlyInterestRate * upperSum;
-        double underSum = upperSum - 1;
-
-        double totalSum = upperSumFinal / underSum;
-
-        double mortgage = principal *  totalSum;
+        double mortgage = calculateMortgage(principal, annualInterestRate, period);
 
         
         /*
@@ -84,5 +68,28 @@ public class mortgageCalculator {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String totalMortgage = currency.format(mortgage);   
         System.out.println("Mortgage: " + totalMortgage);
+    }
+
+    public static double calculateMortgage(
+            int principal, 
+            float annualInterest, 
+            byte years) {
+
+        // Create constants
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterestRate = annualInterest / PERCENT / MONTHS_IN_YEAR; // Calculate monthly Interest Rate
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR); // Calculate monthly payments
+
+        float monthlyInterestRatePlusOne = 1 + monthlyInterestRate;
+        double upperSum = Math.pow(monthlyInterestRatePlusOne, numberOfPayments); 
+        double upperSumFinal = monthlyInterestRate * upperSum;
+        double underSum = upperSum - 1;
+
+        double totalSum = upperSumFinal / underSum;
+
+        double mortgage = principal *  totalSum;
+        return mortgage;
     }
 }
